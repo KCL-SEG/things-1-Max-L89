da2332626b3593e9e85c951295a4a4d84b0a7c45
Lin Ma <malin04809@gmail.com>
2023-12-06 23:50:40 +0000

    modify the models.py

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import Model
+
 
 # Create your models here.
+class thing(Model):
+    name=models.CharField()
+    description=models.TextField()
+    quantity=models.IntegerField()
+
