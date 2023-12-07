46fab5b7de8e3e5813d8598d949d72e959040a82
Lin Ma <malin04809@gmail.com>
2023-12-07 00:07:44 +0000

    make migrations

--- things/models.py
+++ things/models.py
@@ -6,9 +6,9 @@ from django.db.models import Model
 # Create your models here.
 
 
-class thing(Model):
+class Thing(Model):
     name=models.CharField(unique=True,blank=False,max_length=30)
-    description=models.TextField(unique=False,blank=True,max_length=120)
+    description=models.CharField(unique=False,blank=True,max_length=120)
     quantity=models.IntegerField(unique=False,validators=[
         MinValueValidator(0),
         MaxValueValidator(100)
