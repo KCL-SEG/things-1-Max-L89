cf760b8255c64fb2b2744248c2a7e0132818759e
Lin Ma <malin04809@gmail.com>
2023-12-07 00:06:02 +0000

    make migrations

--- things/models.py
+++ things/models.py
@@ -10,7 +10,7 @@ class thing(Model):
     name=models.CharField(unique=True,blank=False,max_length=30)
     description=models.TextField(unique=False,blank=True,max_length=120)
     quantity=models.IntegerField(unique=False,validators=[
-        MinValueValidator(1),
+        MinValueValidator(0),
         MaxValueValidator(100)
 
     ])
